1479e270d6eb3b9dabc538217879d7a8bd338ea5
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-08-01 08:27:07 +0200

    Initial Commit

---
+++ Actors/ActorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActorsApp.swift
+//  Actors
+//
+//  Created by Skorobogatow, Christian on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ActorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
